db950d0bee53b55495d1a1816fd75e6226733fcf
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2024-03-05 19:43:50 -0700

    Completed isLeap function

--- leap/leap.js
+++ leap/leap.js
@@ -3,29 +3,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const isLeap = () => {
+export function isLeap(input) {
   let isLeapYear = false;
   let year = input;
   
   if (year % 4 === 0) {
-    if (year % 100 ==== 0) {
-      if (year % 400 ===) {
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
         isLeapYear = true;
       }
     } else {
       isLeapYear = true;
-    }
-  //then yesleapyearmaybe
-  
-     //if year % 100 = 0
-     //then if year % 400
-         //then isLeapYear = true;
-         //else isLeapYear = false (no action);
-     //else isLeepyear = true;
-  
-  //else no action (still not leap year)
-}
-
+      }
+  }
 return isLeapYear;
 
 };
