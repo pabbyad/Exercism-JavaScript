33880483abe6154083de9f105b9c7a4edee0ad29
philipabbyad <96091530+philipabbyad@users.noreply.github.com>
2024-03-05 21:30:16 -0700

    Refactored. Removed if block

--- leap/leap.js
+++ leap/leap.js
@@ -1,14 +1,3 @@
-//
-// This is only a SKELETON file for the 'Leap' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const isLeap = (year) => {
-  let isLeapYear = false;
-  
-  if ((year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0)) {
-        isLeapYear = true;
-  }
-
-return isLeapYear;
+  return (year % 4 === 0) && (year % 100 !== 0 || year % 400 === 0);
 };
